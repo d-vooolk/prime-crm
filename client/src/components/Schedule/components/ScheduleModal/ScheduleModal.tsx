498beb998824e40bd9a233655509f135bcbade48
d-vooolk <volk.dmitrii@otr.ru>
2025-08-25 01:01:27 +0300

    add modal content

--- client/src/components/Schedule/components/ScheduleModal/ScheduleModal.tsx
+++ client/src/components/Schedule/components/ScheduleModal/ScheduleModal.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import {Card, Form, Input, Modal, TimePicker} from "antd";
+import React, {useState} from 'react';
+import {Button, Card, DatePicker, Form, Image, Input, Modal, TimePicker, Typography} from "antd";
 import styles from "./ScheduleModal.module.scss";
-import Calendar from "../../../Calendar";
 import dayjs from "dayjs";
+import {DownOutlined} from "@ant-design/icons";
+import TextArea from "antd/es/input/TextArea";
 
 const timeFormat = "HH:mm";
+const defaultTime = '09:00';
 
 const ScheduleModal = ({isOpen, closeModal}: { isOpen: boolean, closeModal: () => void }) => {
+    const [isOpenRecordDetails, setIsOpenRecordDetails] = useState(false);
+
+    const openRecordDetails = () => setIsOpenRecordDetails(true);
+    const closeRecordDetails = () => setIsOpenRecordDetails(false);
+
     return (
         <Modal
             open={isOpen}
@@ -14,26 +21,36 @@ const ScheduleModal = ({isOpen, closeModal}: { isOpen: boolean, closeModal: () =
             onOk={closeModal}
             className={styles.modalWrapper}
             width={1200}
+            okText="Сохранить"
+            cancelText="Отменить"
         >
             <div className={styles.modalContent}>
-                <div className={styles.modalCalendar}>
-                    <Calendar/>
-                </div>
                 <Card className={styles.modalCardContent}>
                     <Form className={styles.formWrapper}>
-                        <Form.Item
-                            label="Время записи"
-                            name="time"
-                            layout="vertical"
-                        >
-                            <TimePicker defaultValue={dayjs('09:00', timeFormat)} format={timeFormat} />
-                        </Form.Item>
+                        <div className={styles.dateTimeWrapper}>
+                            <Form.Item
+                                label="Дата записи"
+                                name="date"
+                                layout="vertical"
+                            >
+                                <DatePicker />
+                            </Form.Item>
+
+                            <Form.Item
+                                label="Время записи"
+                                name="time"
+                                layout="vertical"
+                            >
+                                <TimePicker defaultValue={dayjs(defaultTime, timeFormat)} format={timeFormat}/>
+                            </Form.Item>
+                        </div>
+
                         <Form.Item
                             label="ФИО"
                             name="clientName"
                             layout="vertical"
                         >
-                            <Input/>
+                            <Input placeholder="Волк Дмитрий Иванович" />
                         </Form.Item>
 
                         <Form.Item
@@ -41,7 +58,7 @@ const ScheduleModal = ({isOpen, closeModal}: { isOpen: boolean, closeModal: () =
                             name="phone"
                             layout="vertical"
                         >
-                            <Input/>
+                            <Input placeholder="+375 (29) 999-99-99" />
                         </Form.Item>
 
                         <Form.Item
@@ -49,15 +66,15 @@ const ScheduleModal = ({isOpen, closeModal}: { isOpen: boolean, closeModal: () =
                             name="car"
                             layout="vertical"
                         >
-                            <Input/>
+                            <Input placeholder="BMW 5-er E60"/>
                         </Form.Item>
 
                         <Form.Item
-                            label="Перечень работ"
-                            name="works"
+                            label="Комментарий"
+                            name="comment"
                             layout="vertical"
                         >
-                            <Input/>
+                            <TextArea rows={4} placeholder="Особенности заказа"/>
                         </Form.Item>
                     </Form>
                 </Card>
@@ -65,94 +82,129 @@ const ScheduleModal = ({isOpen, closeModal}: { isOpen: boolean, closeModal: () =
                 <Card className={styles.modalCardContent}>
                     <Form className={styles.formWrapper}>
                         <Form.Item
-                            label="Рег знак"
-                            name="carNumber"
+                            label="Перечень работ"
+                            name="works"
                             layout="vertical"
                         >
-                            <Input/>
-                        </Form.Item>
-
-                        <Form.Item
-                            label="Год выпуска"
-                            name="carYear"
-                            layout="vertical"
-                        >
-                            <Input/>
-                        </Form.Item>
-
-                        <Form.Item
-                            label="Пробег"
-                            name="carMilleage"
-                            layout="vertical"
-                        >
-                            <Input/>
-                        </Form.Item>
-
-                        <Form.Item
-                            label="Предв. стоимость"
-                            name="firstPrice"
-                            layout="vertical"
-                        >
-                            <Input/>
-                        </Form.Item>
-
-                        <Form.Item
-                            label="Мастер-приёмщик"
-                            name="serviceMan"
-                            layout="vertical"
-                        >
-                            <Input/>
-                        </Form.Item>
-
-                        <Form.Item
-                            label="Обнаруженные недостатки"
-                            name="wrongDetails"
-                            layout="vertical"
-                        >
-                            <Input/>
-                        </Form.Item>
-
-                        <Form.Item
-                            label="Обоснование доб. стоимости"
-                            name="whyAddPrice"
-                            layout="vertical"
-                        >
-                            <Input/>
-                        </Form.Item>
-
-                        <Form.Item
-                            label="Итоговая стоимость"
-                            name="fullPrice"
-                            layout="vertical"
-                        >
-                            <Input/>
-                        </Form.Item>
-
-                        <Form.Item
-                            label="Дата окончания работ"
-                            name="dateOfWorkDone"
-                            layout="vertical"
-                        >
-                            <Input/>
-                        </Form.Item>
-
-                        <Form.Item
-                            label="Гарантия"
-                            name="warranty"
-                            layout="vertical"
-                        >
-                            <Input/>
-                        </Form.Item>
-
-                        <Form.Item
-                            label="Модель модулей"
-                            name="modulesModel"
-                            layout="vertical"
-                        >
-                            <Input/>
+                            <Input placeholder="Установка Bi-Led модулей"/>
                         </Form.Item>
                     </Form>
                 </Card>
+
+                <Card className={styles.modalCardContent}>
+                    {
+                        !isOpenRecordDetails && (
+                            <Typography onClick={() => openRecordDetails()} className={styles.modalText}>
+                                Раскрыть данные записи <DownOutlined />
+                            </Typography>
+                        )
+                    }
+                    {
+                        isOpenRecordDetails && (
+                            <Typography onClick={() => closeRecordDetails()} className={styles.modalText}>
+                                Скрыть данные записи <DownOutlined />
+                            </Typography>
+                        )
+                    }
+                    {
+                        isOpenRecordDetails && (
+                            <Form className={styles.formWrapper}>
+                                <Form.Item
+                                    label="Рег знак"
+                                    name="carNumber"
+                                    layout="vertical"
+                                >
+                                    <Input placeholder="1111 MB-1"/>
+                                </Form.Item>
+
+                                <Form.Item
+                                    label="Год выпуска"
+                                    name="carYear"
+                                    layout="vertical"
+                                >
+                                    <Input placeholder="2020"/>
+                                </Form.Item>
+
+                                <Form.Item
+                                    label="Пробег"
+                                    name="carMilleage"
+                                    layout="vertical"
+                                >
+                                    <Input placeholder="300 000"/>
+                                </Form.Item>
+
+                                <Form.Item
+                                    label="Предв. стоимость"
+                                    name="firstPrice"
+                                    layout="vertical"
+                                >
+                                    <Input placeholder="100"/>
+                                </Form.Item>
+
+                                <Form.Item
+                                    label="Мастер-приёмщик"
+                                    name="serviceMan"
+                                    layout="vertical"
+                                >
+                                    <Input placeholder="Волк Дмитрий Иванович"/>
+                                </Form.Item>
+
+                                <Form.Item
+                                    label="Обнаруженные недостатки"
+                                    name="wrongDetails"
+                                    layout="vertical"
+                                >
+                                    <TextArea rows={4} placeholder="Негерметичность пыльников"/>
+                                </Form.Item>
+
+                                <Form.Item
+                                    label="Обоснование доб. стоимости"
+                                    name="whyAddPrice"
+                                    layout="vertical"
+                                >
+                                    <TextArea rows={4} placeholder="Герметизация корпуса"/>
+                                </Form.Item>
+
+                                <Form.Item
+                                    label="Итоговая стоимость"
+                                    name="fullPrice"
+                                    layout="vertical"
+                                >
+                                    <Input placeholder="100"/>
+                                </Form.Item>
+
+                                <Form.Item
+                                    label="Дата окончания работ"
+                                    name="dateOfWorkDone"
+                                    layout="vertical"
+                                >
+                                    <Input placeholder="30.04.1998"/>
+                                </Form.Item>
+
+                                <Form.Item
+                                    label="Гарантия"
+                                    name="warranty"
+                                    layout="vertical"
+                                >
+                                    <Input placeholder="1 год"/>
+                                </Form.Item>
+
+                                <Form.Item
+                                    label="Модель модулей"
+                                    name="modulesModel"
+                                    layout="vertical"
+                                >
+                                    <Input placeholder="Sanvi F50"/>
+                                </Form.Item>
+                            </Form>
+                        )
+                    }
+                </Card>
+            </div>
+
+            <div className={styles.printButtons}>
+                <Button>Печать заявки</Button>
+                <Button>Печать акта</Button>
             </div>
         </Modal>
     )
